f5a588489585879b05ac202d26a12bbba75df3cd
Pedro Pérez <pedro@zepyrshut.com>
2021-09-15 18:29:50 +0200

    ﻿Correcion clases Entrenador y Usuario II

---
+++ imd/src/main/java/com/arelance/beans/Entrenador.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.arelance.beans;
+
+/**
+ *
+ * @author jorge
+ */
+public class Entrenador {
+    private int identrenador;
+    private String nombre;
+
+    public Entrenador() {
+    }
+
+    public Entrenador(int identrenador, String nombre) {
+        this.identrenador = identrenador;
+        this.nombre = nombre;
+    }
+
+    public int getIdentrenador() {
+        return identrenador;
+    }
+
+    public void setIdentrenador(int identrenador) {
+        this.identrenador = identrenador;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
